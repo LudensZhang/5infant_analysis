1041634b9e2f8b23b7f20781556b567eb3998cbc
iceQHdrop <iceqhdrop@outlook.com>
2021-10-04 21:32:10 +0800

    plot

--- 8folds/box-plot.R
+++ 8folds/box-plot.R
@@ -4,8 +4,21 @@ plot.data = read.csv('plot-data.csv')
 
 p1 = ggplot(plot.data, aes(x=Env, y=ROC.AUC, fill=mode))+
   geom_boxplot()+
-  xlim(c('NB:C', 'NB:V', '4M:C', '4M:V', 
-         '12M:C', '12M:V', '3Y:C', '3Y:V', '5Y:C', '5Y:V','M'))+
+  scale_fill_manual(values = c("#DC143C", "#87CEEB"))+
+  xlim(c('NB(C)', 'NB(V)', '4M(C)', '4M(V)', 
+         '12M(C)', '12M(V)', '3Y(C)', '3Y(V)', '5Y(C)', '5Y(V)','M'))+
   theme_bw()+
-  xlab('')
+  xlab('')+
+  ylab('AUROC')+
+  stat_compare_means(aes(label = ..p.signif..), 
+                     hide.ns = TRUE,
+                     method = "wilcox.test",
+                     label.y = 0.95,
+                     size =10
+                     )+
+  theme(text=element_text(size = 20),
+        legend.title = element_blank(),
+        legend.position = c(0.9, 0.1))
 p1
+
+ggsave('r-plot-data.jpg', height = 5, width = 10)
