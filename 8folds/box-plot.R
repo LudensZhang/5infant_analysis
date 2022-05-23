64bc1309a2aa90958dd0bae2b4d56a6b12936312
iceQHdrop <iceqhdrop@outlook.com>
2022-05-23 13:47:30 +0800

    edit box plot

--- 8folds/box-plot.R
+++ 8folds/box-plot.R
@@ -16,9 +16,14 @@ p1 = ggplot(plot.data, aes(x=Env, y=ROC.AUC, fill=mode))+
                      label.y = 0.95,
                      size =10
                      )+
-  theme(text=element_text(size = 20),
+  theme(text=element_text(size = 10),
+        axis.text = element_text(size = 10, color = 'black'),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        legend.background = element_blank(),
         legend.title = element_blank(),
-        legend.position = c(0.9, 0.1))
+        legend.position = c(0.92, 0.11))
 p1
 
-ggsave('r-plot-data.jpg', height = 5, width = 10)
+ggsave('r-plot-data.jpg', height = 100, width = 200, units = 'mm')
